28acf46d2e761d4e72b5d798fdb24dd86545bfbb
slimetamer1 <sarmiento.kyle322@gmail.com>
2025-03-02 22:37:10 +0800

    Delete frontend directory

--- frontend/src/pages/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import Forms from "../components/Forms"
-
-const LoginPage = () => {
-  return (
-    <Forms route="/api/token/" method="login" />
-  )
-}
-
-export default LoginPage
--- frontend/src/pages/Register.tsx
+++
@@ -1,9 +0,0 @@
-import Forms from "../components/Forms"
-
-const Register = () => {
-  return (
-    <Forms route="/api/user/register/" method="register" />
-  )
-}
-
-export default Register
